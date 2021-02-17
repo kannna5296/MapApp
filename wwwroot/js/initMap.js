fde310bfbe8dc42700394024d7564b944c0ceb35
kannna5296 <kannnahacks5296@gmail.com>
2021-02-17 23:04:20 +0900

    first

---
+++ wwwroot/js/initMap.js
@@ -0,0 +1,21 @@
+﻿//初期にとりあえず表示させる用
+window.onload = function initMap() {
+    //都庁
+    var mapCenter = new google.maps.LatLng(35.6896385, 139.689912);
+
+    var mapOptions = {
+        center: mapCenter,
+        zoom: 15,
+        minZoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var maker = new google.maps.Maker({
+        position: mapCenter,
+        animation: google.maps.animation.BOUNCE,
+    });
+
+    maker.setMap(map);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿function initMap() {
+
+    //getElementById を使う方法の記述例
+    var input_message = document.getElementById("place").value;
+
+    // Geocoderインスタンス化
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+        'address': input_message
+    },
+        function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                var latlng = results[0].geometry.location;
+
+                var mapOptions = {
+                    center: latlng,
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+                var maker = new google.maps.Maker({
+                    position: latlng,
+                    animation: google.maps.animation.BOUNCE,
+                });
+
+
+                maker.setMap(map);
+            } else {
+                alert('場所を取得できませんでした');
+            }
+        });
+};
